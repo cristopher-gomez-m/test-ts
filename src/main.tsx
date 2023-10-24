9a6188d46ee438c3561d692b2c6fe82322be3614
Cristopher Gomez <sonrisita011@hotmail.com>
2023-10-23 20:49:20 -0500

    prueba

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React, { lazy, Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const App = lazy(
+  () =>
+    new Promise<{ default: React.ComponentType<any> }>((resolve) => {
+      setTimeout(() => resolve(import("./App")), 0);
+    })
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
